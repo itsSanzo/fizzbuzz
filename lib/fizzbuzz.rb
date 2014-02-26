dafbd911e4d635d5025e1b65392081e3da6b74c2
itsmurasaki <itsmurasaki@gmail.com>
2014-02-26 12:38:55 +0000

    third

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -13,3 +13,15 @@ end
 def is_multiple_of_fifteen?(number)
   is_multiple_of?(number, 15)
 end
+
+def play(number)
+ return "Fizz" if is_multiple_of_three?(number)
+ return "Buzz" if is_multiple_of_five?(number)
+ return "Fizzbuzz" if is_multiple_of_fifteen?(number)
+ return number
+end
+
+puts play(52)
+puts play(3)
+puts play(10)
+puts play(30)
