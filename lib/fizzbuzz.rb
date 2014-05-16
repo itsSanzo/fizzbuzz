0d3796f77c503b72d664ce18b0be29479e9ab793
itsmurasaki <itsmurasaki@gmail.com>
2014-05-16 23:26:12 +0100

    added play method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,8 @@
 def fizzbuzz(number)
-  if is_divisible_by_fifteen?(number)
-    return "FizzBuzz" 
-  elsif is_divisible_by_five?(number)
-    return "Buzz"
-  elsif is_divisible_by_three?(number)
-    return "Fizz"
-  else
-    number
-  end
+  return "FizzBuzz" if is_divisible_by_fifteen?(number)
+  return "Fizz" if is_divisible_by_three?(number)
+  return "Buzz" if is_divisible_by_five?(number)
+  number
 end
 
 def is_divisible_by(number, divider)
@@ -25,3 +20,10 @@ end
 def is_divisible_by_three?(number)
   is_divisible_by(number,3)
 end
+
+def play_fizzbuzz(from_number, to_number)
+  from_number.upto(to_number).map {|number| puts fizzbuzz(number)}
+end
+
+# example! 
+play_fizzbuzz(1,100)
