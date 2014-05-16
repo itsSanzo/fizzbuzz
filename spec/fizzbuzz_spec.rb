0ec3d0ec1d10b1ce39e9b4d7d2ce50e29f6e03c6
itsmurasaki <itsmurasaki@gmail.com>
2014-05-16 22:12:10 +0100

    is divisible by five

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,12 +2,16 @@ require "fizzbuzz"
 
 describe "Fizzbuzz" do
 
-  it "knows 3 if it's divisible by 3" do
+  it "knows 3 is divisible by 3" do
     expect(is_divisible_by_three?(3)).to be_true
   end
 
-  it "knows 9 if it's divisible by 3" do
+  it "knows 9 is divisible by 3" do
     expect(is_divisible_by_three?(9)).to be_true
+  end
+
+  it "knows 5 is divisible by 5" do
+    expect(is_divisible_by_five?(5)).to be_true
   end
 
 end
